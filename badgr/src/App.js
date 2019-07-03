c279029c5d24cd9e67c47bf19e6d9873b954449c
Freek De Sagher (win) <freek.de.sagher21@gmail.com>
2019-07-03 15:03:19 +0200

    Create badgeclass function in badgr test project

--- badgr/src/App.js
+++ badgr/src/App.js
@@ -4,6 +4,19 @@ import axios from 'axios';
 
 const badgr_api = `https://api.badgr.io/`;
 
+function getBase64Image(img) {
+  var canvas = document.createElement("canvas");
+  canvas.width = img.width;
+  canvas.height = img.height;
+  var ctx = canvas.getContext("2d");
+  ctx.drawImage(img, 0, 0);
+  var dataURL = canvas.toDataURL("image/png");
+  let newVarianble = dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+  console.log("Check this ---->",newVarianble);
+  return newVarianble;
+}
+
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -40,17 +53,22 @@ class App extends Component {
   componentDidUpdate(prevProps, prevState) {
     if (prevState.access_token === null) {
       // this.createIssuer();
+    
       this.getIssuers();
+      // this.getBadgeClasses();
       // console.log(issuers);
       // let id = issuers[0].entityId;
       // console.log(id);
     }
-    if(this.state.issuers !== null && this.state.issuers.length > 0){
-      let issuers = this.state.issuers;
-      while(issuers.length > 0){
-        this.deleteIssuer(issuers[issuers.length-1].entityId);
-        issuers.pop();
-      }
+    // if(this.state.issuers !== null && this.state.issuers.length > 0){
+    //   let issuers = this.state.issuers;
+    //   while(issuers.length > 0){
+    //     this.deleteIssuer(issuers[issuers.length-1].entityId);
+    //     issuers.pop();
+    //   }
+    // }
+    if(this.state.issuers !== null && this.state.issuers.length > 0) {
+      this.createBadgeClass();
     }
   }
   getBackpack() {
@@ -98,6 +116,29 @@ class App extends Component {
       axios.delete(badgr_api + 'v2/issuers/' + issuerID, { headers: { Authorization: "Bearer " + this.state.access_token } }).
       then(res => console.log(res))
       .catch(err => console.error(err));
+  }
+
+  createBadgeClass(){
+    let img = new Image();
+    img.src = "https://firebasestorage.googleapis.com/v0/b/gentle-student.appspot.com/o/Badges%2Fbadge_entre-spirit_1.png?alt=media"
+    let base64image = getBase64Image(img);
+    let data = {
+      name: "toffe badge 2.0",
+      description: "heel toffe badge om te hebben, nog maar is",
+      issuer: this.state.issuers[0].entityId,
+      image: base64image,
+      criteriaNarrative: "narrative"
+    }
+    axios.post(badgr_api + 'v2/badgeclasses',data,  { headers: { Authorization: "Bearer " + this.state.access_token } } )
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+  }
+
+  getBadgeClasses() {
+  
+    axios.get(badgr_api +"v2/badgeclasses", { headers: { Authorization: "Bearer " + this.state.access_token } })
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
   }
 
   render() {
