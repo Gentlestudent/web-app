48394699febc1005498316aaa7c3aedcd3661170
Freek De Sagher (win) <freek.de.sagher21@gmail.com>
2019-07-03 13:57:56 +0200

    Added remove issuer function to badgr test project

--- badgr/src/App.js
+++ badgr/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
@@ -12,8 +11,12 @@ class App extends Component {
     this.state = {
       access_token: null,
       expiration: -1,
-      refresh_token: null
+      refresh_token: null,
+      issuers: null
     };
+
+    this.getIssuers = this.getIssuers.bind(this);
+
   }
   componentDidMount() {
     // let data = {username: 'van-driessche-maxime@hotmail.com', password: 'osocosoc'};
@@ -27,18 +30,27 @@ class App extends Component {
           access_token: res.data.access_token,
           expiration: res.data.expires_in,
           refresh_token: res.data.refresh_token
-        });
-      }).catch(err => {
+        }
+        );
+      }
+      ).catch(err => {
         console.log(err);
       });
   }
-  componentDidUpdate() {
-    if (this.state.access_token != null) {
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.access_token === null) {
       // this.createIssuer();
       this.getIssuers();
       // console.log(issuers);
       // let id = issuers[0].entityId;
       // console.log(id);
+    }
+    if(this.state.issuers !== null && this.state.issuers.length > 0){
+      let issuers = this.state.issuers;
+      while(issuers.length > 0){
+        this.deleteIssuer(issuers[issuers.length-1].entityId);
+        issuers.pop();
+      }
     }
   }
   getBackpack() {
@@ -54,7 +66,8 @@ class App extends Component {
   createIssuer() {
     console.log("creating issuer");
     let data = {
-      name: "string",
+      name: "test user",
+      entityId: "companyhisemailcom",
       email: "freek.de.sagher21@gmail.com",
       description: "test",
       url: "https://www.google.com"
@@ -67,17 +80,26 @@ class App extends Component {
         // console.log(this.state.access_token);
       });
   }
+
   getIssuers(){
     console.log("fetching issuers");
     axios.get(badgr_api + 'v2/issuers', { headers: { Authorization: "Bearer " + this.state.access_token } })
       .then(res => {
         console.log(res);
-        return res.data;
+        this.setState({issuers: res.data.result});
       }).catch(err => {
         console.log(err);
         // console.log(this.state.access_token);
       });
   }
+
+  deleteIssuer(issuerID) {
+      console.log("deleting issuer");
+      axios.delete(badgr_api + 'v2/issuers/' + issuerID, { headers: { Authorization: "Bearer " + this.state.access_token } }).
+      then(res => console.log(res))
+      .catch(err => console.error(err));
+  }
+
   render() {
     return (
       <div>
