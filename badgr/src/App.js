f7aa9cd7eac4ee424d9f0013a25eb18d963fdba9
mrvdries <van-driessche-maxime@hotmail.com>
2019-07-03 15:45:49 +0200

    upload badgeclass

--- badgr/src/App.js
+++ badgr/src/App.js
@@ -4,18 +4,19 @@ import axios from 'axios';
 
 const badgr_api = `https://api.badgr.io/`;
 
-function getBase64Image(img) {
-  var canvas = document.createElement("canvas");
-  canvas.width = img.width;
-  canvas.height = img.height;
-  var ctx = canvas.getContext("2d");
-  ctx.drawImage(img, 0, 0);
-  var dataURL = canvas.toDataURL("image/png");
-  let newVarianble = dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-  console.log("Check this ---->",newVarianble);
-  return newVarianble;
+function toDataUrl(url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    var reader = new FileReader();
+    reader.onloadend = function () {
+      callback(reader.result);
+    }
+    reader.readAsDataURL(xhr.response);
+  };
+  xhr.open('GET', url);
+  xhr.responseType = 'blob';
+  xhr.send();
 }
-
 
 class App extends Component {
   constructor(props) {
@@ -53,7 +54,7 @@ class App extends Component {
   componentDidUpdate(prevProps, prevState) {
     if (prevState.access_token === null) {
       // this.createIssuer();
-    
+
       this.getIssuers();
       // this.getBadgeClasses();
       // console.log(issuers);
@@ -67,7 +68,7 @@ class App extends Component {
     //     issuers.pop();
     //   }
     // }
-    if(this.state.issuers !== null && this.state.issuers.length > 0) {
+    if (this.state.issuers !== null && this.state.issuers.length > 0) {
       this.createBadgeClass();
     }
   }
@@ -90,7 +91,7 @@ class App extends Component {
       description: "test",
       url: "https://www.google.com"
     };
-    axios.post(badgr_api + 'v2/issuers', data , { headers: { Authorization: "Bearer " + this.state.access_token } })
+    axios.post(badgr_api + 'v2/issuers', data, { headers: { Authorization: "Bearer " + this.state.access_token } })
       .then(res => {
         console.log(res);
       }).catch(err => {
@@ -99,12 +100,12 @@ class App extends Component {
       });
   }
 
-  getIssuers(){
+  getIssuers() {
     console.log("fetching issuers");
     axios.get(badgr_api + 'v2/issuers', { headers: { Authorization: "Bearer " + this.state.access_token } })
       .then(res => {
         console.log(res);
-        this.setState({issuers: res.data.result});
+        this.setState({ issuers: res.data.result });
       }).catch(err => {
         console.log(err);
         // console.log(this.state.access_token);
@@ -112,33 +113,34 @@ class App extends Component {
   }
 
   deleteIssuer(issuerID) {
-      console.log("deleting issuer");
-      axios.delete(badgr_api + 'v2/issuers/' + issuerID, { headers: { Authorization: "Bearer " + this.state.access_token } }).
+    console.log("deleting issuer");
+    axios.delete(badgr_api + 'v2/issuers/' + issuerID, { headers: { Authorization: "Bearer " + this.state.access_token } }).
       then(res => console.log(res))
       .catch(err => console.error(err));
   }
 
-  createBadgeClass(){
-    let img = new Image();
-    img.src = "https://firebasestorage.googleapis.com/v0/b/gentle-student.appspot.com/o/Badges%2Fbadge_entre-spirit_1.png?alt=media"
-    let base64image = getBase64Image(img);
-    let data = {
-      name: "toffe badge 2.0",
-      description: "heel toffe badge om te hebben, nog maar is",
-      issuer: this.state.issuers[0].entityId,
-      image: base64image,
-      criteriaNarrative: "narrative"
-    }
-    axios.post(badgr_api + 'v2/badgeclasses',data,  { headers: { Authorization: "Bearer " + this.state.access_token } } )
-    .then(res => console.log(res))
-    .catch(err => console.log(err));
+  createBadgeClass() {
+    let self = this;
+    toDataUrl("https://raw.githubusercontent.com/FreekDS/Wof/master/res/player0.png", function (myBase64) {
+      console.log(myBase64); // myBase64 is the base64 string
+      let data = {
+        name: "toffe badge 2.0",
+        description: "heel toffe badge om te hebben, nog maar is",
+        issuer: self.state.issuers[0].entityId,
+        image: myBase64,
+        criteriaNarrative: "narrative"
+      }
+      axios.post(badgr_api + 'v2/badgeclasses', data, { headers: { Authorization: "Bearer " + self.state.access_token } })
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
+    });
   }
 
   getBadgeClasses() {
-  
-    axios.get(badgr_api +"v2/badgeclasses", { headers: { Authorization: "Bearer " + this.state.access_token } })
-    .then(res => console.log(res))
-    .catch(err => console.log(err));
+
+    axios.get(badgr_api + "v2/badgeclasses", { headers: { Authorization: "Bearer " + this.state.access_token } })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
   }
 
   render() {
