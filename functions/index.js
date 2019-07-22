1f04fac2074969b079c891905769c9c551c44d19
Freek De Sagher (win) <freek.de.sagher21@gmail.com>
2019-07-22 13:36:18 +0200

    Added email function

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@
  * BADGR docs: https://api.badgr.io/docs/v2/
  */
 
+ 
 
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
@@ -11,9 +12,12 @@ const nodemailer = require('nodemailer');
 
 admin.initializeApp();
 
+// BADGR FUNCTIONALITY
+
 /**
  * Creates the URL for the BADGR API
- * @param {string} path 
+ * @param {string} path path extension
+ * @returns full badgr URL
  */
 function BADGR_PATH(path) {
     let base = "https://api.badgr.io";
@@ -23,7 +27,8 @@ function BADGR_PATH(path) {
 }
 
 /**
- * Gets the header from the firestore and return it
+ * Fetch access token and creates header
+ * @returns json object containing the Authorization header
  */
 async function getHeader() {
     try {
@@ -78,60 +83,6 @@ exports.refreshToken = functions.https.onCall(async () => {
     }
 
     return true;
-});
-async function refreshToken() {
-    let document = admin.firestore().collection('BadgrAuth').doc('auth');
-    try {
-        console.log("Trying to refresh access token");
-        const fireDoc = await document.get();
-        let docData = fireDoc.data();
-        let refreshToken = docData.refreshToken;
-        let url = BADGR_PATH('o', 'token');
-        let data = "grant_type=refresh_token&²refresh_token=" + refreshToken.toString();
-
-        let res = await axios.post(url, data);
-        let newData = {
-            accessToken: res.data.access_token,
-            refreshToken: res.data.refresh_token
-        }
-
-        document.update(
-            {
-                accessToken: newData.accessToken,
-                refreshToken: newData.refreshToken
-            }
-        ).then(res => {
-            console.log("Successfully updated access token");
-            return newData.accessToken;
-        })
-            .catch(err => {
-                throw err;
-            });
-
-    } catch (err) {
-        return console.error(err);
-    }
-
-    return true;
-}
-
-// TEST FUNCTION
-exports.functionTest = functions.https.onCall(async (data, context) => {
-    try {
-        let test = "username=freek.de.sagher21@gmail.com&password=summerofcode2019";
-        console.log("fetching tokens");
-        const res = await axios.post("https://api.badgr.io/" + 'o/token', test);
-        console.log("Token time!", res.data);
-        return {
-            accessToken: res.data.access_token,
-            expiration: res.data.expires_in,
-            refreshToken: res.data.refresh_token
-        };
-    }
-    catch (err) {
-        return console.log("Error happened :'(", err);
-    }
-
 });
 
 /**
@@ -278,7 +229,6 @@ exports.createAssertion = functions.https.onCall(async (data) => {
 
 // EMAIL FUNCTIONALITY
 
-
 /**
  * Create a transporter to send the email
  */
@@ -290,13 +240,20 @@ let transporter = nodemailer.createTransport({
         ciphers: 'SSLv3'
     },
     auth: {
-        user: functions.config().mailer.email,
-        pass: functions.config().mailer.pass
+        user: functions.config().mailer.email,  // Cloud variable
+        pass: functions.config().mailer.pass    // Cloud variable
     }
 });
 
-
-
+/**
+ * Sends email to issuer when participant subscribes to learning opportunity
+ * @param {Object} data Input data for the email
+ * Data needs:
+ *  1. opportunityTitle: title of the opportunity
+ *  2. participantName: name of the participant
+ *  3. issuerEmail: email address of the issuer
+ *  4. participantEmail: email address of the participant
+ */
 exports.notifyIssuer = functions.https.onCall((data) => {
 
     let opportunityTitle = data.opportunityTitle;
@@ -325,7 +282,16 @@ exports.notifyIssuer = functions.https.onCall((data) => {
     return sendEmail(mailOptions)
 });
 
-
+/**
+ * Sends email when someone subscribes to his quest
+ * @param {Object} data Input data for the email
+ * Data needs:
+ *  1. takerName: name of the quest taker
+ *  2. giverEmail: email of the quest giver
+ *  3. questTitle: title of the quest
+ *  4. giverName: name of the quest giver
+ *  5. questId: ID of the quest
+ */
 exports.notifyQuestGiver = functions.https.onCall(data => {
     let takerName = data.takerName;
     let giverEmail = data.giverEmail;
@@ -351,7 +317,17 @@ exports.notifyQuestGiver = functions.https.onCall(data => {
     return sendMyMail(mailOptions);
 });
 
-
+/**
+ * Sends email when quest taker is chosen by quest giver.
+ * @param {Object}data Input data for the email
+ * Data needs:
+ *  1. giverName: name of the quest giver
+ *  2. giverEmail: email of the quest giver
+ *  3. takerName: name of the quest taker
+ *  4. takerEmail: email of the quest taker
+ *  5. questTitle: title of the quest 
+ *  6. questId: id of the quest
+ */
 exports.notifyQuestTaker = functions.https.onCall(data => {
     let giverName = data.giverName;
     let giverEmail = data.giverEmail;
@@ -365,7 +341,7 @@ exports.notifyQuestTaker = functions.https.onCall(data => {
     let html = "<p>Beste " + takerName + ",</p>" +
         "<p>" + giverName + " heeft jou gekozen om hem/haar te helpen bij '" + questTitle + "'.</p>" +
         "<p>Neem contact op met hem/haar via " + giverEmail + ", breng de quest tot een goed einde</p>" +
-        "<p>en verdien zo jouw token!</p>"
+        "<p>en verdien zo jouw token!</p><br>"
     "<p>Met vriendelijke groet,</p>" +
         "<p>Team Gentlestudent</p>";
 
@@ -380,8 +356,43 @@ exports.notifyQuestTaker = functions.https.onCall(data => {
     return sendMyMail(mailOptions);
 });
 
+/**
+ * Sends email when quest taker receives his token.
+ * @param {Object} data Input data.
+ * Data needs:
+ *  1. giverName: name of the quest giver
+ *  2. takerName: name of the quest taker
+ *  3. takerEmail: email address of quest taker
+ *  4. questTitle: title of the quest
+ */
+exports.notifyTokenReceived = functions.https.onCall(data => {
+    let giverName = data.giverName;
+    let takerName = data.takerName;
+    let takerEmail = data.takerEmail;
+    let questTitle = data.questTitle;
 
+    let subject = "[QUEST] Token verdiend!";
+    let html = "<p>Beste " + takerName + ",</p>" +
+        "<p>Proficiat, je hebt succesvol de token behaald voor de quest '" + questTitle + "' van " + giverName + ".</p>" +
+        "<p>Je kan deze bekijken in de mobiele applicatie. </p>" +
+        "<br><p>Met vriendelijke groet,</p>"
+        + "<p>Team Gentlestudent</p>";
 
+    let mailOptions = {
+        from: "Gentlestudent <" + functions.config().mailer.email + ">",
+        to: "freek.de.sagher21@gmail.com", // TODO swap out
+        subject: subject,
+        text: "",
+        html: html
+    }
+
+    return sendMyMail(mailOptions);
+});
+
+/**
+ * Sends the actual email
+ * @param {Object} mailOptions email options
+ */
 async function sendMyMail(mailOptions) {
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
