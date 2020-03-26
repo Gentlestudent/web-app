7f38e5508726a1e034d99b5a3dccf05411bbde10
Michiel Leyman <michiel.leyman@gmail.com>
2020-03-26 13:16:16 +0100

    Add import alias

--- v2/next.config.js
+++ v2/next.config.js
@@ -1,10 +1,17 @@
 /* eslint-disable import/no-extraneous-dependencies */
 require('dotenv').config();
 
+const path = require('path');
+
 const ENV = process.env.ENV || 'development';
 const publicENV = require(`./environments`)(ENV);
 
 module.exports = {
+  webpack: (config) => {
+    // root import alias
+    config.resolve.alias['@'] = path.resolve(`${__dirname}/`);
+    return config;
+  },
   env: {
     ...publicENV
     // manually add keys from .env files as such
