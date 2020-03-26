fe28e39c3e406d34396ba3f80fd240ef285080c2
Michiel Leyman <michiel.leyman@gmail.com>
2020-03-26 20:08:49 +0100

    Add AuthProvider component

---
+++ v2/components/auth-provider.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+import AuthContext from '@/context/auth';
+import { createAuthSubscription } from '@/api/auth';
+
+const AuthProvider = ({ children }) => {
+  const [authState, setAuthState] = useState({
+    authStatusReported: false,
+    isUserSignedIn: false,
+    currentUser: null
+  });
+
+  useEffect(() => {
+    createAuthSubscription((user) => {
+      setAuthState({ ...authState, authStatusReported: true });
+      if (!user) return;
+      setAuthState({
+        ...authState,
+        isUserSignedIn: true,
+        currentUser: user
+      });
+    });
+  }, []);
+
+  return (
+    <AuthContext.Provider value={authState}>
+      {authState.authStatusReported && children}
+    </AuthContext.Provider>
+  );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired
+};
+
+export default AuthProvider;
---
+++ v2/context/auth.js
@@ -0,0 +1,7 @@
+import { createContext } from 'react';
+
+export default createContext({
+  authStatusReported: false,
+  isUserSignedIn: false,
+  currentUser: null
+});
