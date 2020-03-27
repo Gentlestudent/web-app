463b642de29c3ad15a1f43ec69e53ac69269be6f
Michiel Leyman <michiel.leyman@gmail.com>
2020-03-27 14:06:16 +0100

    Add Input component

---
+++ v2/components/UI/input.jsx
@@ -0,0 +1,55 @@
+import PropTypes from 'prop-types';
+import { colors } from '@/styles/constants';
+import Icon from './icon';
+
+const Input = ({ icon, placeholder, type, ...rest }) => {
+  return (
+    <>
+      <div>
+        {icon && (
+          <i>
+            <Icon name={icon} />
+          </i>
+        )}
+        <input id="email" type={type} placeholder={placeholder} {...rest} />
+      </div>
+      <style jsx>
+        {`
+          div {
+            width: 100%;
+            position: relative;
+            display: flex;
+            align-items: center;
+            background-color: ${colors.white};
+            box-shadow: 0 1px 1px ${colors.gray};
+            border-radius: 0.3rem;
+          }
+          input {
+            width: 100%;
+            height: 100%;
+            color: ${colors.copy};
+            background: transparent;
+            border: 0;
+            padding: 1rem;
+          }
+          i {
+            color: ${colors.copy};
+          }
+        `}
+      </style>
+    </>
+  );
+};
+
+Input.defaultProps = {
+  placeholder: '',
+  icon: null
+};
+
+Input.propTypes = {
+  placeholder: PropTypes.string,
+  icon: PropTypes.string,
+  type: PropTypes.string.isRequired
+};
+
+export default Input;
