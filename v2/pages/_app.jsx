f2e366f1e0751cbec9d44dd8e03ca6c2f1913313
Michiel Leyman <michiel.leyman@gmail.com>
2020-05-05 17:41:49 +0200

    Replace AuthProvider by auth hook

--- v2/pages/_app.jsx
+++ v2/pages/_app.jsx
@@ -1,13 +1,15 @@
-import AuthProvider from '../components/auth-provider';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../api/firebase';
+
 import globalStyles from '../styles/global';
 
 const App = ({ Component, pageProps }) => {
+  const [user, loading, error] = useAuthState(auth);
+
   return (
     <>
-      <AuthProvider>
-        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-        <Component {...pageProps} />
-      </AuthProvider>
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Component {...pageProps} />
       <style jsx global>
         {globalStyles}
       </style>
