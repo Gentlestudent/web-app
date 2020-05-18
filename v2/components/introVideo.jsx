77a6aaa5472eeb860398f369781fa2c29b484b84
Hektor Misplon <hektor.misplon@gmail.com>
2020-05-18 13:28:48 +0000

    Make intro video component responsive

--- v2/components/introVideo.jsx
+++ v2/components/introVideo.jsx
@@ -1,16 +1,10 @@
+import { breakpoints } from '../assets/styles/constants';
+
 const IntroVideo = () => (
   <div className="intro-video">
     <iframe
       title="Intro video"
       src="https://arteveldehogeschool.cloud.panopto.eu/Panopto/Pages/Embed.aspx?id=dc47c1a8-68b9-413b-812a-aa1400a18754&v=1"
-      style={{
-        objectFit: 'cover',
-        maxWidth: 720,
-        width: '100%',
-        maxHeight: 405,
-        overflow: 'visible'
-      }}
-      width="100%"
       frameBorder="0"
       allowFullScreen
       allow="autoplay"
@@ -18,9 +12,22 @@ const IntroVideo = () => (
     <style jsx>
       {`
         .intro-video {
-          padding: 0;
           margin-top: 5vh;
           margin-bottom: 1rem;
+        }
+
+        iframe {
+          padding: 10rem;
+          width: 100vw;
+          max-width: 120rem;
+          max-height: calc(120rem / 1.6);
+          height: calc(100vw / 1.6);
+        }
+
+        @media (max-width: ${breakpoints.small}) {
+          iframe {
+            padding: 0;
+          }
         }
       `}
     </style>
