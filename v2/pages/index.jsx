2c3506226a6e32e4162d98307b3a1232b891f319
Hektor Misplon <hektor.misplon@gmail.com>
2020-05-18 13:29:13 +0000

    Add index page base layout

--- v2/pages/index.jsx
+++ v2/pages/index.jsx
@@ -2,17 +2,50 @@ import { useContext } from 'react';
 import Head from 'next/head';
 import AuthContext from '../context/auth';
 import { Heading } from '../components/UI';
+import IntroVideo from '../components/introVideo';
+import eyecatcher from '../assets/img/home/eyecatcher.jpg';
+import { colors } from '../assets/styles/constants';
 
 const Home = () => {
   const { isUserSignedIn, currentUser } = useContext(AuthContext);
   return (
-    <>
+    <div className="scroll-snap">
       <Head>
         <title>Welkom | Gentlestudent</title>
       </Head>
-      <Heading level={1} color="#000" title="Aan de slag met Gentlestudent" />
-      <Heading level={2} color="#000" title="“Verken je stad, help je buren.”" />
-    </>
+      <section id="one">
+        <Heading level={1} color="#fff" title="Aan de slag met Gentlestudent" />
+        <Heading level={2} color="#fff" title="“Verken je stad, help je buren.”" />
+        <IntroVideo />
+      </section>
+      <section id="two">{/* learning opportunities */}</section>
+      <section id="three">{/* about app + footer */}</section>
+      <style jsx>
+        {`
+          /* TODO: add mandatory y scroll on html element */
+          section {
+            scroll-snap-align: start;
+            min-height: 100vh;
+          }
+
+          #one {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            background-image: url(${eyecatcher});
+            background-size: cover;
+          }
+
+          #two {
+          }
+
+          #three {
+            background: ${colors.grayLight};
+          }
+        `}
+      </style>
+    </div>
   );
 };
 
