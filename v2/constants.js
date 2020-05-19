e8466eefe1aaf58eee184070d5efb296ebc01ac5
Hektor Misplon <hektor.misplon@gmail.com>
2020-05-19 17:26:47 +0000

    Add opportunity route constants

--- v2/constants.js
+++ v2/constants.js
@@ -1,3 +1,23 @@
 export const routes = {
-  home: '/'
+  home: '/',
+
+  /*
+   * Opportunities
+   */
+
+  /* public */
+  opportunities: '/opportunities',
+  opportunity: '/opportunities/[id]',
+
+  /* user */
+  userOpportunities: '/user/opportunities',
+
+  /* issuer */
+  issuerCreateOpportunity: '/issuer/create-opportunity',
+  issuerOpportunities: '/issuer/opportunities',
+  // issuerOpportunity: '/issuer/opportunities/id',
+  // editOpportunity: '/issuer/opportunities/id/edit',
+
+  /* admin */
+  validateOpportunity: '/admin/validate-opportunity'
 };
