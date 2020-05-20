298b13ddad4dfdde64dc8c84e5859d8fd426999f
Hektor Misplon <hektor.misplon@gmail.com>
2020-05-20 15:15:52 +0000

    Bind form group values

--- v2/pages/issuer/opportunities/create.jsx
+++ v2/pages/issuer/opportunities/create.jsx
@@ -1,32 +1,55 @@
+import { useInput } from '../../../hooks';
 import { Heading, FormGroup } from '../../../components/UI';
 
-export default () => (
-  <div>
-    <Heading title="Maak opportunity" />
-    <FormGroup
-      label="Titel"
-      info="Schrijf hier een motiverende en uitdagende titel voor jouw leerkans"
-      type="text"
-      name="title"
-      placeholder="Titel"
-      required
-    />
-    <FormGroup
-      label="Titel"
-      info="Schrijf hier een motiverende en uitdagende titel voor jouw leerkans"
-      type="dropdown"
-      name="title"
-      options={['01', '02', '03']}
-      placeholder="Selecteer domein"
-      required
-    />
-    <FormGroup
-      label="Beschrijving"
-      info="Schrijf hier een motiverende en uitdagende titel voor jouw leerkans"
-      type="textarea"
-      name="title"
-      placeholder="Beschrijving"
-      required
-    />
-  </div>
-);
+export default () => {
+  const { value: title, bind: bindTitle } = useInput('');
+  const { value: domain, bind: bindDomain } = useInput('');
+  const { value: description, bind: bindDescription } = useInput('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const opportunity = {
+      title,
+      domain,
+      description
+    };
+
+    console.log(opportunity);
+  };
+
+  return (
+    <div>
+      <Heading title="Maak opportunity" />
+      <FormGroup
+        label="Titel"
+        info="Schrijf hier een motiverende en uitdagende titel voor jouw leerkans"
+        type="text"
+        name="title"
+        placeholder="Titel"
+        required
+        {...bindTitle}
+      />
+      <FormGroup
+        label="Titel"
+        info="Schrijf hier een motiverende en uitdagende titel voor jouw leerkans"
+        type="dropdown"
+        name="title"
+        options={['01', '02', '03']}
+        placeholder="Selecteer domein"
+        required
+        {...bindDomain}
+      />
+      <FormGroup
+        label="Beschrijving"
+        info="Schrijf hier een motiverende en uitdagende titel voor jouw leerkans"
+        type="textarea"
+        name="description"
+        placeholder="Beschrijving"
+        required
+        {...bindDescription}
+      />
+      <button onClick={handleSubmit}>Create</button>
+    </div>
+  );
+};
