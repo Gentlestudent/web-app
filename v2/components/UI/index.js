4f916a094476328c6bae8aedc3c026076764f4d3
Hektor Misplon <hektor.misplon@gmail.com>
2020-05-20 12:37:52 +0000

    Add textarea component

--- v2/components/UI/index.js
+++ v2/components/UI/index.js
@@ -4,3 +4,4 @@ export { default as Input } from './input';
 export { default as Heading } from './heading';
 export { default as Card } from './card';
 export { default as Dropdown } from './dropdown';
+export { default as TextArea } from './textarea';
---
+++ v2/components/UI/textarea.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import { colors } from '../../assets/styles/constants';
+
+const TextArea = ({ name, placeholder, rows, cols, required }) => (
+  <>
+    <textarea name={name} placeholder={placeholder} required={required} />
+    <style jsx>{`
+      textarea {
+        padding: 2rem 2rem 0 2rem;
+        border: 1px solid ${colors.gray};
+        width: 100%;
+        border-radius: 1rem;
+      }
+    `}</style>
+  </>
+);
+
+TextArea.propTypes = {
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  required: PropTypes.bool
+};
+
+export default TextArea;
