b73dcf79c9af16e8c39c4d00655460bf2a342c9a
Hektor Misplon <hektor.misplon@gmail.com>
2020-05-22 15:33:02 +0000

    Convert to multi-step form

--- v2/pages/issuer/opportunities/create.jsx
+++ v2/pages/issuer/opportunities/create.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import Router from 'next/router';
 
 import { useInput } from '../../../hooks';
@@ -28,14 +30,41 @@ const GoBack = () => (
   </div>
 );
 
+const FormSection = ({ title, children }) => (
+  <section>
+    <div className="section-header">
+      <Heading title={title} level={2} />
+    </div>
+    {children}
+    <style jsx>{`
+      section {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        margin: 2rem 0;
+        padding: 2rem;
+        border-radius: 2rem;
+        box-shadow: 0 0.5rem 1rem 0.2rem rgba(0, 0, 0, 0.1);
+      }
+
+      .section-header {
+        margin-bottom: 3rem;
+        padding: 1rem 0;
+        border-bottom: 1px solid ${colors.primary};
+      }
+    `}</style>
+  </section>
+);
+
 export default () => {
-  /*
-   * TODO: Convert to multi-step form
-   *
-   * 1. Current step number
-   * 2. Current step validation
-   * 3. Current step "save"
-   */
+  const [step, setStep] = useState(0);
+
+  const validateStep = () => {
+    console.log(step);
+  };
+
+  const nextStep = () => step < steps.length - 1 && setStep(step + 1);
+  const previousStep = () => step > 0 && setStep(step - 1);
 
   const { value: title, bind: bindTitle } = useInput('');
   const { value: domain, bind: bindDomain } = useInput('');
@@ -62,118 +91,161 @@ export default () => {
      */
   };
 
+  const steps = [
+    <FormSection title="Algemeen">
+      <FormGroup
+        label="Titel"
+        info="Schrijf hier een motiverende en uitdagende titel voor jouw leerkans"
+        type="text"
+        name="title"
+        placeholder="Titel"
+        required
+        {...bindTitle}
+      />
+      <FormGroup
+        label="Domein"
+        info={
+          <>
+            Duid aan binnen welk domein je leerkans valt.
+            {` `}
+            <a
+              href="https://firebasestorage.googleapis.com/v0/b/gentle-student.appspot.com/o/Formopportunity%2Fpdf%2FDomeinen.pdf?alt=media"
+              target="_blank"
+            >
+              Hier
+            </a>
+            {` `}
+            vind je een uitgebreide omschrijving van de verschillende categorieën.
+          </>
+        }
+        type="dropdown"
+        name="title"
+        options={DOMAINS}
+        placeholder="Selecteer domein"
+        required
+        {...bindDomain}
+      />
+      <FormGroup
+        label="Beschrijving"
+        info="Algemene beschrijving van de leerkans die je organisatie biedt. 
+          Geef ook achtergrondinformatie over je instelling om de leerkans binnen je algemene werking te kaderen."
+        type="textarea"
+        name="description"
+        placeholder="Volledige beschrijving van de leerkans"
+        required
+        {...bindDescription}
+      />
+    </FormSection>,
+
+    <FormSection title="Verwachtingen & niveau">
+      <FormGroup
+        label="Verwachtingen"
+        info="Verwachtingen van student voor jouw organisatie. Kennen, kunnen of nodige acties van de student voor voltooiing van deze leerkans? 
+          Stem deze goed af met het niveau."
+        type="textarea"
+        name="expectations"
+        placeholder="Korte beschrijving van wat er verwacht wordt"
+        required
+        {...bindExpectations}
+      />
+      <FormGroup
+        label="Niveau"
+        info={
+          <>
+            Selecteer moeilijkheidsgraad van de leerkans.{' '}
+            <a
+              href="https://firebasestorage.googleapis.com/v0/b/gentle-student.appspot.com/o/Formopportunity%2Fpdf%2FNiveaus.pdf?alt=media"
+              target="_blank"
+            >
+              Zie verschillende moeilijkheidsgraden
+            </a>{' '}
+          </>
+        }
+        type="dropdown"
+        name="level"
+        options={LEVELS}
+        placeholder="Selecteer niveau"
+        required
+        {...bindLevel}
+      />
+    </FormSection>,
+    <FormSection title="Leerkans details">
+      <FormGroup
+        type="text"
+        label="Website"
+        info="Weblink binnen de organisatie met info over dit initiatief"
+        placeholder="Website url"
+      />
+      <FormGroup
+        type="email"
+        label="Email contactpersoon"
+        info="Emailadres van de contactpersoon binnen de organisatie"
+        placeholder="Emailadres contactpersoon"
+      />
+    </FormSection>
+  ];
+
   return (
-    <div>
+    <div className="page">
       <GoBack />
       <Heading title="Maak opportunity" />
-      <section>
-        <div className="section-header">
-          <Heading title="Leerkans algemeen" level={2} />
-        </div>
-        <FormGroup
-          label="Titel"
-          info="Schrijf hier een motiverende en uitdagende titel voor jouw leerkans"
-          type="text"
-          name="title"
-          placeholder="Titel"
-          required
-          {...bindTitle}
-        />
-        <FormGroup
-          label="Domein"
-          info={
-            <>
-              Duid aan binnen welk domein je leerkans valt.
-              {` `}
-              <a
-                href="https://firebasestorage.googleapis.com/v0/b/gentle-student.appspot.com/o/Formopportunity%2Fpdf%2FDomeinen.pdf?alt=media"
-                target="_blank"
-              >
-                Hier
-              </a>
-              {` `}
-              vind je een uitgebreide omschrijving van de verschillende categorieën.
-            </>
-          }
-          type="dropdown"
-          name="title"
-          options={DOMAINS}
-          placeholder="Selecteer domein"
-          required
-          {...bindDomain}
-        />
-        <FormGroup
-          label="Beschrijving"
-          info="Algemene beschrijving van de leerkans die je organisatie biedt. 
-          Geef ook achtergrondinformatie over je instelling om de leerkans binnen je algemene werking te kaderen."
-          type="textarea"
-          name="description"
-          placeholder="Volledige beschrijving van de leerkans"
-          required
-          {...bindDescription}
-        />
-        <FormGroup
-          label="Verwachtingen"
-          info="Verwachtingen van student voor jouw organisatie. Kennen, kunnen of nodige acties van de student voor voltooiing van deze leerkans? 
-          Stem deze goed af met het niveau."
-          type="textarea"
-          name="expectations"
-          placeholder="Korte beschrijving van wat er verwacht wordt"
-          required
-          {...bindExpectations}
-        />
-        <FormGroup
-          label="Niveau"
-          info={
-            <>
-              Selecteer moeilijkheidsgraad van de leerkans.{' '}
-              <a
-                href="https://firebasestorage.googleapis.com/v0/b/gentle-student.appspot.com/o/Formopportunity%2Fpdf%2FNiveaus.pdf?alt=media"
-                target="_blank"
-              >
-                Zie verschillende moeilijkheidsgraden
-              </a>{' '}
-            </>
-          }
-          type="dropdown"
-          name="level"
-          options={LEVELS}
-          placeholder="Selecteer niveau"
-          required
-          {...bindLevel}
-        />
-      </section>
-      <section>
-        <div className="section-header">
-          <Heading title="Leerkans details" level={2} />
-        </div>
-        <FormGroup
-          type="text"
-          label="Website"
-          info="Weblink binnen de organisatie met info over dit initiatief"
-          placeholder="Website url"
-        />
-        <FormGroup
-          type="email"
-          label="Email contactpersoon"
-          info="Emailadres van de contactpersoon binnen de organisatie"
-          placeholder="Emailadres contactpersoon"
-        />
-      </section>
-      <Button onClick={handleSubmit}>Maak leerkans</Button>
+      {steps[step]}
+      <div className="steps">
+        {step > 0 && (
+          <Button onClick={previousStep} className="back">
+            Terug
+          </Button>
+        )}
+        {steps.map((s, i) => (
+          <div className="step button-container">
+            <a className={`step_number ${step === i ? 'active' : ''}`} onClick={() => setStep(i)}>
+              {i + 1}
+            </a>
+            <span>Algemeen</span>
+          </div>
+        ))}
+        {step < steps.length - 1 ? (
+          <Button onClick={nextStep}>Ga door</Button>
+        ) : (
+          <Button onClick={handleSubmit}>Bevestig</Button>
+        )}
+      </div>
       <style jsx>{`
-        section {
-          max-width: 60rem;
-          margin: 2rem 0;
-          padding: 2rem;
-          border-radius: 2rem;
-          box-shadow: 0 0.5rem 1rem 0.2rem rgba(0, 0, 0, 0.1);
+        .page {
+          display: flex;
+          flex-direction: column;
+          min-height: 100vh;
         }
 
-        .section-header {
-          margin-bottom: 3rem;
-          padding: 1rem 0;
-          border-bottom: 1px solid ${colors.primary};
+        .steps {
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+          padding: 2rem;
+        }
+
+        .step {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        .step_number {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: 4rem;
+          height: 4rem;
+          margin-right: 1rem;
+          border-radius: 1rem;
+          background: ${colors.white};
+          color: ${colors.primary};
+          border: 1px solid ${colors.primary};
+        }
+
+        .active {
+          background: ${colors.primary};
+          color: ${colors.white};
         }
       `}</style>
     </div>
