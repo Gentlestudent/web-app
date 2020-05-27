71096460e66948aa561c0c13fc1b114472bf21c1
Hektor Misplon <hektor.misplon@gmail.com>
2020-05-27 19:19:59 +0200

    Add custom form steps hook

---
+++ v2/hooks/useSteps.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+export default (initialValue) => {
+  const [steps, setSteps] = useState(initialValue);
+  const [currentStep, setCurrentStep] = useState(0);
+
+  return {
+    steps,
+    setSteps,
+    currentStep: steps[currentStep],
+    nextStep: () => currentStep < steps.length - 1 && setCurrentStep(currentStep + 1),
+    previousStep: () => currentStep > 0 && setCurrentStep(currentStep - 1)
+  };
+};
