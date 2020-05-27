773e9864799ea9f1afd8ef31a96bcd7d2d180fc0
Hektor Misplon <hektor.misplon@gmail.com>
2020-05-27 13:56:51 +0200

    Add button icon

--- v2/components/UI/button.jsx
+++ v2/components/UI/button.jsx
@@ -2,57 +2,61 @@ import PropTypes from 'prop-types';
 import { colors } from '../../assets/styles/constants';
 import Icon from './icon';
 
-const Button = ({
-  icon = null,
-  type = 'button',
-  isLoading,
-  href = null,
-  primary = false,
-  children,
-  ...rest
-}) => {
-  return (
-    <>
-      {/* see https://github.com/yannickcr/eslint-plugin-react/issues/1555 */}
-      {/* eslint-disable-next-line react/button-has-type */}
-      <button className={primary ? 'primary' : ''} type={type} {...rest}>
-        {children}
-      </button>
-      <style jsx>
-        {`
-          button {
-            background-color: ${colors.orange};
-            border: 0;
-            cursor: pointer;
-            padding: 0.8rem 3rem;
-            color: ${colors.white};
-            font-weight: bold;
-            border-radius: 0.3rem;
-          }
+const Button = ({ icon, type, isLoading, href, primary, children, ...rest }) => (
+  <>
+    {/* see https://github.com/yannickcr/eslint-plugin-react/issues/1555 */}
+    {/* eslint-disable-next-line react/button-has-type */}
+    <button className={primary ? 'primary' : ''} type={type} {...rest}>
+      {icon && (
+        <i>
+          <Icon name={icon} />
+        </i>
+      )}
+      {children}
+    </button>
+    <style jsx>
+      {`
+        button {
+          display: flex;
+          background-color: ${colors.orange};
+          border: 0;
+          cursor: pointer;
+          padding: 0.8rem 3rem;
+          color: ${colors.white};
+          font-weight: bold;
+          border-radius: 0.3rem;
+        }
 
-          button.primary {
-            background-color: ${colors.primary};
-          }
+        button.primary {
+          background-color: ${colors.primary};
+        }
 
-          button.primary:hover {
-            cursor: pointer;
-            transition: all 200ms ease-in-out;
-            color: ${colors.primary};
-            background-color: transparent;
-          }
-        `}
-      </style>
-    </>
-  );
-};
+        button.primary:hover {
+          cursor: pointer;
+          transition: all 200ms ease-in-out;
+          color: ${colors.primary};
+          background-color: transparent;
+        }
+
+        i {
+          margin-right: 1rem;
+        }
+      `}
+    </style>
+  </>
+);
 
 Button.propTypes = {
   icon: PropTypes.string,
   type: PropTypes.oneOf(['button', 'submit', 'reset']),
   isLoading: PropTypes.bool,
   href: PropTypes.string,
-  children: PropTypes.node.isRequired,
-  primary: PropTypes.bool
+  primary: PropTypes.bool,
+  children: PropTypes.node.isRequired
+};
+
+Button.defaultProps = {
+  type: 'button'
 };
 
 export default Button;
