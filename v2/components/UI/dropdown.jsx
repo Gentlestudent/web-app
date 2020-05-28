940cec5bcfe3b1696c1eebdf447f5ef768b5b259
Hektor Misplon <hektor.misplon@gmail.com>
2020-05-28 12:53:36 +0200

    Fix lint errors

--- v2/components/UI/dropdown.jsx
+++ v2/components/UI/dropdown.jsx
@@ -6,25 +6,27 @@ const Dropdown = ({ name, placeholder, options, ...rest }) => (
     <select name={name} {...rest}>
       {placeholder && <option>{placeholder}</option>}
       {options.map((option, id) => (
-        <option key={id}>{option}</option>
+        <option key={id.toString()}>{option}</option>
       ))}
     </select>
-    <style jsx>{`
-      select {
-        width: 100%;
-        border: 1px solid ${colors.gray};
-        border-radius: 1rem;
-        padding: 2rem;
-        background: transparent;
-      }
-    `}</style>
+    <style jsx>
+      {`
+        select {
+          width: 100%;
+          border: 1px solid ${colors.gray};
+          border-radius: 1rem;
+          padding: 2rem;
+          background: transparent;
+        }
+      `}
+    </style>
   </>
 );
 
 Dropdown.propTypes = {
   name: PropTypes.string.isRequired,
   placeholder: PropTypes.string,
-  options: PropTypes.array.isRequired
+  options: PropTypes.arrayOf(PropTypes.object).isRequired
 };
 
 export default Dropdown;
