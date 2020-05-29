c349154e0f448b9478593f253f4ba8c7a24e9b22
h <hektor.misplon@gmail.com>
2020-05-29 14:15:20 +0200

    Clean

--- v2/hooks/useForm.js
+++ v2/hooks/useForm.js
@@ -1,24 +1,19 @@
 import { useState, useEffect } from 'react';
-import { useInput } from '.';
-import { inputTypes } from '../constants';
 import { validator } from '../validate';
 
 export default (fields) => {
   const [values, setValues] = useState();
   const [errors, setErrors] = useState([]);
-  const [validating, setValidating] = useState(false);
   const [submitting, setSubmitting] = useState(false);
 
   const isValid = errors.length === 0;
 
   const validate = () => {
-    setValidating(true);
     setErrors([
       ...fields
         .map(({ name }) => validator[name](values[name]).error)
         .filter((field) => field !== null)
     ]);
-    setValidating(false);
   };
 
   useEffect(() => {
@@ -39,8 +34,7 @@ export default (fields) => {
 
     handleSubmit: (e) => {
       e.preventDefault();
-      console.log(`Submitting: ${JSON.stringify(values)}`);
-      console.log(`Errors: ${JSON.stringify(errors)}`);
+      if (isValid) setSubmitting(true);
     }
   };
 };
