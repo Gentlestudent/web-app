d813e5e8cc2dc28ec75c7a288654cb3ba0a81249
h <hektor.misplon@gmail.com>
2020-06-02 09:05:29 +0200

    Return values

--- v2/hooks/useSteps.js
+++ v2/hooks/useSteps.js
@@ -1,12 +1,21 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default (initialValue) => {
+  const [values, setValues] = useState([]);
   const [steps, setSteps] = useState(initialValue);
   const [currentStep, setCurrentStep] = useState(0);
+  const [currentStepValues, setCurrentStepValues] = useState([]);
+
+  useEffect(() => {
+    setValues([...currentStepValues]);
+  }, currentStepValues);
 
   return {
     steps,
     setSteps,
+    currentStepValues,
+    setCurrentStepValues,
+    values,
     currentStep: steps[currentStep],
     nextStep: (e) => {
       e.preventDefault();
