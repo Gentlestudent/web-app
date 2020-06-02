6b3cddcda92b05979aaff69f6a4d736301e4c0c9
h <hektor.misplon@gmail.com>
2020-06-02 09:02:29 +0200

    Add stepper component

--- v2/pages/opportunities/create.jsx
+++ v2/pages/opportunities/create.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Router from 'next/router';
 
@@ -34,17 +35,19 @@ const GoBack = () => (
   </button>
 );
 
-const Form = ({ title, fields, children }) => {
-  const { props } = useForm(fields);
+const Form = ({ title, fields, children, errors, ...rest }) => {
+  const error = (name) => errors.find((error) => Object.keys(error)[0] === name);
+
+  console.log(error('title'));
   return (
-    <form {...props}>
+    <form {...rest}>
       <div className="section-header">
         <Heading level={2} title={title} />
       </div>
       {fields &&
-        fields.map((field) => {
+        fields.map((field, i) => {
           const { name } = field;
-          return <FormGroup key={name} {...field} />;
+          return <FormGroup key={name} error={errors[i]} {...field} />;
         })}
       {children}
       <style jsx>
@@ -68,25 +71,54 @@ const Form = ({ title, fields, children }) => {
   );
 };
 
+const Stepper = ({ currentStep, previousStep, nextStep, onSubmit }) => {
+  const { formProps, values } = useForm(currentStep.fields);
+  return (
+    <Form {...currentStep} {...formProps} values={values} onSubmit={onSubmit}>
+      <div className="stepper">
+        <Button type="button" onClick={previousStep}>
+          Stap terug
+        </Button>
+        <Button type="submit" onClick={(e) => nextStep(e)}>
+          Ga door
+        </Button>
+        <style jsx>
+          {`
+            .stepper {
+              padding: 1rem;
+              display: flex;
+              justify-content: space-between;
+              position: fixed;
+              left: 0;
+              right: 0;
+              bottom: 0;
+              background: ${colors.white};
+              box-shadow: 0 0.5rem 1rem 0.5rem rgba(0, 0, 0, 0.2);
+              margin: 0 calc(15vw - 4.5rem);
+            }
+
+            @media (max-width: ${breakpoints.medium}) {
+              .stepper {
+                margin: 0;
+              }
+            }
+          `}
+        </style>
+      </div>
+    </Form>
+  );
+};
+
 export default () => {
-  const { currentStep, nextStep, previousStep } = useSteps(OPPORTUNITY_STEPS);
+  const steps = useSteps(OPPORTUNITY_STEPS);
   return (
     <Layout>
       <GoBack />
       <div className="page">
         <div className="page-header">
           <Heading title="Nieuwe leerkans" />
+          <Stepper {...steps} onSubmit={(e) => console.log(e)} />
         </div>
-        <Form {...currentStep}>
-          <div className="stepper">
-            <Button type="button" onClick={previousStep}>
-              Stap terug
-            </Button>
-            <Button type="submit" onSubmit={(e) => nextStep(e)}>
-              Ga door
-            </Button>
-          </div>
-        </Form>
       </div>
       <style jsx>
         {`
@@ -101,26 +133,9 @@ export default () => {
             padding: 3.2rem;
           }
 
-          .stepper {
-            position: relative;
-            padding: 2rem;
-            display: flex;
-            margin-top: auto;
-            justify-content: space-between;
-          }
-
           @media (max-width: ${breakpoints.medium}) {
             .page {
               margin: 0;
-            }
-
-            .stepper {
-              position: fixed;
-              left: 0;
-              right: 0;
-              bottom: 0;
-              background: ${colors.white};
-              box-shadow: 0 0.5rem 1rem 0.5rem rgba(0, 0, 0, 0.2);
             }
           }
         `}
@@ -133,4 +148,10 @@ Form.propTypes = {
   title: PropTypes.string,
   fields: PropTypes.arrayOf(PropTypes.object),
   children: PropTypes.element
+};
+
+Stepper.propTypes = {
+  currentStep: PropTypes.object,
+  previousStep: PropTypes.func,
+  nextStep: PropTypes.func
 };
