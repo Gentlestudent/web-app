ba719b22c413a721c4831b101c6f0d41f0b9afa5
h <hektor.misplon@gmail.com>
2020-06-03 17:02:53 +0200

    Show steps left on submit button

--- v2/components/form-with-steps.jsx
+++ v2/components/form-with-steps.jsx
@@ -7,33 +7,20 @@ import Form from './form';
 
 const FormWithSteps = ({ steps, onCompleteAll }) => {
   const { currentStep, isFinalStep, stepForward, stepBack, stepTo } = useSteps(steps.length);
-  const flattenedFields = steps.reduce((all, step) => all.concat(step.fields), []);
-  const stepTitles = steps.reduce((all, step) => all.concat(step.title), []);
 
   /*
-   * Opportunity is result of all form values
+   * Result contains values of all form steps
    */
-  const [opportunity, setOpportunity] = useState({});
+  const [result, setResult] = useState({});
 
   /*
-   * Validate
-   * Save & continue if valid, else show errors
+   * Add current step values to result & continue
    */
   const submitStep = (values) => {
     /*
-     * TODO: Validate - return errors if errors
-     */
-    //
-
-    /*
-     * TODO: save form state client side
-     */
-    //
-
-    /*
      * Add current step values to multi step form result
      */
-    setOpportunity({ ...opportunity, ...values });
+    setResult({ ...result, ...values });
 
     /*
      * Continue
@@ -42,16 +29,21 @@ const FormWithSteps = ({ steps, onCompleteAll }) => {
   };
 
   /*
-   * Add opportunity to firestore
+   * Add result to firestore
    */
-  const handleSubmit = () => {
-    onCompleteAll(opportunity);
-  };
+  const handleSubmit = () => onCompleteAll(result);
+
+  /*
+   * Create array containing step titles
+   */
+  const stepTitles = steps.reduce((all, step) => all.concat(step.title), []);
 
   /*
    * Current step props
    */
   const { title, fields } = steps[currentStep];
+
+  const stepsLeft = steps.length - currentStep - 1;
 
   return (
     <Form onSubmit={submitStep} title={title} fields={fields}>
@@ -59,17 +51,20 @@ const FormWithSteps = ({ steps, onCompleteAll }) => {
         <Button type="button" onClick={stepBack}>
           Stap terug
         </Button>
-        {stepTitles.map((title, index) => (
-          <Button key={index} onClick={() => stepTo(index)}>
-            {`${index + 1}. ${title}`}
-          </Button>
+        {stepTitles.map((title, i) => (
+          <p key={i} className={i === currentStep && 'active'}>
+            {title}
+          </p>
         ))}
         {!isFinalStep ? (
           <Button type="submit" onSubmit={submitStep}>
+            <div className="step-indicator">
+              <span>{stepsLeft}</span>
+            </div>
             Ga door
           </Button>
         ) : (
-          <Button type="submit" onClick={handleSubmit}>
+          <Button type="submit" onSubmit={handleSubmit}>
             Bevestig
           </Button>
         )}
@@ -87,6 +82,28 @@ const FormWithSteps = ({ steps, onCompleteAll }) => {
             background: ${colors.white};
             box-shadow: 0 0.5rem 1rem 0.5rem rgba(0, 0, 0, 0.2);
             margin: 0 calc(15vw - 4.5rem);
+          }
+
+          .active {
+            color: ${colors.primary};
+          }
+
+          .step-indicator {
+            position: absolute;
+            font-size: 1.2rem;
+            display: flex;
+            height: 3rem;
+            width: 3rem;
+            top: -0.5rem;
+            right: -0.5rem;
+            border-radius: 1.5rem;
+            color: ${colors.orange};
+            border: 1px solid ${colors.orange};
+            background: ${colors.white};
+          }
+
+          .step-indicator > span {
+            margin: auto;
           }
 
           @media (max-width: ${breakpoints.medium}) {
