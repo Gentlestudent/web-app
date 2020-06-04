bb92530031e2949fc8f0a34f45d422cc087a4100
Michiel Leyman <michiel.leyman@gmail.com>
2020-06-04 14:47:20 +0200

    Also initialize firebase storage and functions

--- v2/src/api/firebase.js
+++ v2/src/api/firebase.js
@@ -1,14 +1,17 @@
 import * as firebase from 'firebase/app';
+import 'firebase/storage';
 import 'firebase/auth';
-import 'firebase/database';
+import 'firebase/functions';
 import 'firebase/firestore';
 
 const config = process.env.FIREBASE;
 
 export const app = !firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
 export const auth = firebase.auth();
+export const storage = firebase.storage();
+export const functions = firebase.functions();
+export const { Timestamp } = firebase.firestore;
 
-export const { RecaptchaVerifier } = firebase.auth;
 auth.useDeviceLanguage();
 
 export const firestore = firebase.firestore();
