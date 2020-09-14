e21a75446f6c4e65be521ed694002555041d8d68
Sarah Van Den Berghe <sarah.van.den.berghe@student.howest.be>
2020-09-14 17:36:18 +0200

    privacy page

---
+++ v2/src/pages/privacy/index.jsx
@@ -0,0 +1,160 @@
+import Container from '../../components/container';
+import { Heading, List } from '../../components/UI';
+
+export default () => {
+  return (
+    <>
+      <Container text>
+        <Heading title="Privacyverklaring Gentlestudent" level={1} marginTop />
+        <p>
+          Gentlestudent hecht veel belang aan de bescherming van uw persoonlijke levenssfeer en
+          respecteert de privacywetgeving (GDPR) ter zake. Wat dit concreet wilt zeggen, vindt u
+          hieronder terug.
+        </p>
+        <Heading title="Privacyverklaring Gentlestudent" level={2} />
+        <List>
+          <li>
+            Gentlestudent verzamelt enkel persoonlijke gegevens die u zelf aan ons meedeeltvia
+            registratie als gebruiker of issuer op het platform of via de app.
+          </li>
+          <li>
+            De Gentlestudent app vraagt naar de toestemming om locatievoorzieningen in te schakelen,
+            maar deze registreert de locatie van de gebruiker niet. Deze dient enkel om op de kaart
+            weer te geven waar de leerkansen zich bevinden. Zonder deze toestemming kan de kaart
+            niet ingeladen worden op de app.
+          </li>
+        </List>
+        <Heading title="Welke persoonlijke gegevens verwerkt Gentlestudent?" level={2} />
+        <List>
+          <li>
+            Gentlestudent verzamelt enkel persoonlijke gegevens die nodig zijn zodat de werking van
+            Gentlestudent kan gegarandeerd worden
+          </li>
+          <li>
+            Gentlestudent verzamelt verschillende gegevens. Voor onderstaande acties moeten
+            verplicht volgende gegevens ingevuld worden:
+          </li>
+          <List>
+            <li>
+              Voor het registreren als gebruiker op het platform: Naam/voornaam/instelling/e-mail
+            </li>
+            <li>
+              Voor het registeren als issuer op het platform:
+              Naam/voornaam/organisatie/e-mail/organisatienaam die verschijnt op de Open
+              Badge/Volledige naam van de organisatie/website van de organisatie/adresgegevens
+            </li>
+          </List>
+        </List>
+        <Heading
+          title="Wanneer en waarom verwerkt Gentlestudent persoonlijke gegevens?"
+          level={2}
+        />
+        <p>Gentlestudent verwerkt deze gegevens om:</p>
+        <List>
+          <li>
+            De gebruiker de contactgegevens van de issuer te kunnen bezorgen zodat de leerkans
+            succesvol kan uitgevoerd worden
+          </li>
+          <li>
+            De issuer de contactgegevens van de student te kunnen bezorgen zodat de leerkans
+            succesvol kan uitgevoerd worden
+          </li>
+          <li>
+            Wanneer een leerkans werd afgewerkt een open badge te kunnen genereren met de
+            noodzakelijke metadata:
+          </li>
+          <List>
+            <li>gegevens van de issuer</li>
+            <li>gegevens van de gebruiker</li>
+            <li>criteria uit de leerkans</li>
+            <li>datum van uitgave</li>
+          </List>
+        </List>
+        <Heading title="Hoe behandelt Gentlestudent persoonlijke gegevens?" level={2} />
+        <List>
+          <li>
+            Persoonlijke gegevens worden nooit openbaar gemaakt en niet opgeslagen langer dan
+            noodzakelijk.
+          </li>
+          <li>
+            Uw gegevens worden enkel gebruikt in functie van de het kunnen gebruikmaken van
+            Gentlestudent.
+          </li>
+          <li>
+            Gentlestudent geeft uw contactgegevens nooit door aan derden zonder uw expliciete
+            toestemming.
+          </li>
+          <li>
+            Gentlestudent geeft uw contactgegevens nooit door aan derden voor commerciële
+            doeleinden.
+          </li>
+        </List>
+        <Heading title="Hoe lang bewaart Gentlestudent persoonlijke gegevens?" level={2} />
+        <List>
+          <li>
+            Gentlestudent verbindt zich ertoe uw gegevens niet langer te bewaren dan noodzakelijk
+          </li>
+          <li>
+            Zolang een gebruiker Gentlestudent wenst te gebruiken zullen de gegevens bewaard blijven
+          </li>
+          <li>
+            Gentlestudent verzamelt bepaalde gegevens op een geaggregeerde manier om de eigen
+            werking te evalueren en bij te sturen (bv. het rapporteren over: het aantal gebruikers,
+            uitgereikte Open Badges, beschikbare leerkansen). De gegevens voor statistische of
+            wetenschappelijke doeleinden worden altijd anoniem verwerkt. Individuele identificatie
+            van personen is hierbij dus niet aan de orde.
+          </li>
+        </List>
+        <Heading
+          title="Bewaart Gentlestudent mijn persoonlijke gegevens op een veilige manier?"
+          level={2}
+        />
+        <List>
+          <li>
+            Gegevens worden op een zorgvuldige en veilige manier verwerkt en bewaard. Daartoe maakt
+            Gentlestudent gebruik van diverse beveiligingstechnologieën en -maatregelen om uw
+            gegevens op gepaste wijze te beschermen tegen ongeautoriseerde toegang, gebruik, verlies
+            of openbaarmaking. Deze technologieën en -maatregelen worden op geregelde tijdstippen
+            getest en bijgesteld indien nodig.
+          </li>
+          <li>
+            Gentlestudent verwerkt en beheert gegevens via een Firebase databank waar derden niet
+            zomaar toegang tot kunnen verkrijgen.
+          </li>
+          <li>
+            Moest er zich toch een datalek voordoen waarbij uw gegevens betrokken zijn, dan handelt
+            Gentlestudent volgens de wet voorziene omstandigheden, en wordt u persoonlijk
+            verwittigd.
+          </li>
+          <li>
+            Wanneer persoonsgegevens daadwerkelijk worden gedeeld met externe partijen wordt een
+            verwerkersovereenkomstmet de partijafgesloten waarin steeds voldoende (technische)
+            maatregelen moeten worden opgenomen zodat de beveiliging van uw gegevens zo goed als
+            mogelijk gegarandeerd is.
+          </li>
+        </List>
+        <Heading title="Wat kan ik zelf doen i.v.m. mijn persoonlijke gegevens?" level={2} />
+        <List>
+          <li>
+            U kan op elk moment de persoonlijke gegevens die Gentlestudent over u verwerkt, inzien
+            en eventueel onnauwkeurige of onvolledige gegevens laten verbeteren of verwijderen. Neem
+            daarvoor contact op via gentlestudent@arteveldehs.be
+          </li>
+          <li>
+            Wilt u iets wijzigen in uw contactgegevens. Neem daarvoor contact via
+            gentlestudent@arteveldehs.be
+          </li>
+          <li>
+            U heeft het recht ‘vergeten’ te worden. Neem daarvoor contact via
+            gentlestudent@arteveldehs.be
+          </li>
+        </List>
+        <Heading title="Nog vragen of bedenkingen?" level={2} />
+        <p>
+          Aarzel niet om Gentlestudent te contacteren met vragen over het privacybeleid via
+          gentlestudent@arteveldehs.be
+        </p>
+      </Container>
+    </>
+  );
+};
