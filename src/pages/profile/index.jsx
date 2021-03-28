b37ea6a40ff78bdcace51360f1c5487a05916e25
Sarah Van Den Berghe <sarah@littlemissrobot.com>
2021-03-28 23:48:26 +0200

    profile badges

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,9 +1,26 @@
 import { Container, Grid } from '../../components/layout/index';
 import { Heading, Button, BannerSplit } from '../../components/UI';
-import { colors, breakpoints, spacers } from '../../assets/styles/constants';
+import { colors, breakpoints } from '../../assets/styles/constants';
 import { routes } from '../../constants';
 
 export default () => {
+  // TODO: get badges from database (not sure where this can be found in the current structure)
+  const badges = [
+    {
+      title: 'Title',
+      img:
+        'https://firebasestorage.googleapis.com/v0/b/gentle-student.appspot.com/o/Pins%2Fpin_duurzaamheid_2.png?alt=media',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.'
+    },
+    {
+      title: 'Title',
+      img:
+        'https://firebasestorage.googleapis.com/v0/b/gentle-student.appspot.com/o/Pins%2Fpin_wereldburgerschap_1.png?alt=media',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.'
+    }
+  ];
   return (
     <>
       <BannerSplit>
@@ -31,31 +48,27 @@ export default () => {
         <Container>
           <Heading title="Mijn badges" marginTop level={1} />
           <Grid>
-            <div className="badge">
-              <img src="https://firebasestorage.googleapis.com/v0/b/gentle-student.appspot.com/o/Pins%2Fpin_duurzaamheid_2.png?alt=media" />
-              <div>
-                <Heading title="Titeltje" level={2} />
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                  incididunt ut.
-                </p>
-              </div>
-            </div>
-            <div className="badge">
-              <img src="https://firebasestorage.googleapis.com/v0/b/gentle-student.appspot.com/o/Pins%2Fpin_wereldburgerschap_1.png?alt=media" />
-              <div>
-                <Heading title="Titeltje" level={2} />
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                  incididunt ut.
-                </p>
-              </div>
-            </div>
+            {badges.map((badge) => (
+              <>
+                <div className="badge">
+                  <img src={badge.img} alt={badge.title} />
+                  <div>
+                    <Heading title={badge.title} level={2} />
+                    <p>{badge.description}</p>
+                  </div>
+                </div>
+              </>
+            ))}
             <div className="badge">
               <div>
                 <Heading title="Verdien nog meer badges" level={2} />
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                <Button text="Bekijk leerkansen" icon="arrow-right" primary />
+                <p>Neem deel aan leerkansen en verdien nieuwe badges!</p>
+                <Button
+                  href={routes.OPPORTUNITIES}
+                  text="Bekijk leerkansen"
+                  icon="arrow-right"
+                  primary
+                />
               </div>
             </div>
           </Grid>
@@ -68,10 +81,6 @@ export default () => {
             display: flex;
             align-items: center;
             justify-content: space-between;
-          }
-
-          .button--secondary {
-            color: red !important;
           }
 
           .badge {
@@ -100,35 +109,11 @@ export default () => {
             .heading::before {
               width: calc(100% - 5rem);
             }
-            .heading-text {
-              margin-right: 0rem;
-            }
           }
 
           @media (max-width: 900px) {
             .badge {
               grid-column-end: span 12;
-            }
-
-            .heading-content {
-              grid-template-columns: 1fr;
-              grid-gap: 3rem;
-            }
-
-            .heading-image {
-              height: 30rem;
-              width: calc(100% + 3rem);
-              top: 0;
-              left: -3rem;
-              position: absolute;
-            }
-
-            .heading-text {
-              margin-top: 30rem;
-            }
-
-            .heading-content {
-              padding: ${spacers.medium} 0;
             }
 
             .heading::before {
