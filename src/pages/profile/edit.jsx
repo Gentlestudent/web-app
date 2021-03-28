a5dfbee868e9c949dcce08e61b046f19264c75b3
Sarah Van Den Berghe <sarah@littlemissrobot.com>
2021-03-28 23:08:09 +0200

    notifications with text and finishing values

--- src/pages/profile/edit.jsx
+++ src/pages/profile/edit.jsx
@@ -5,102 +5,128 @@ import { Container, Grid } from '../../components/layout/index';
 import { Heading, Button } from '../../components/UI';
 
 const EditProfile = () => {
+  // TODO: get role, depending on role, different info will be shown (getNotifInfo). Text is not final!
+  const getNotifInfo = (role) => {
+    switch (role) {
+      case 'user':
+        return 'Bepaal hoe je meldingen krijgt wanneer je aanvraag om deel te nemen aan een leerkans gereviewed werd.';
+      case 'admin':
+        return 'Bepaal hoe je meldingen krijgt wanneer organisaties een nieuwe leerkans aanmaken.';
+      case 'institution':
+        return 'Bepaal hoe je meldingen krijgt wanneer jouw leerkans werd goedgekeurd en wanneer studenten zich aanmelden aan een leerkans. ';
+      default:
+        return 'Bepaal hoe je meldingen krijgt.';
+    }
+  };
+
+  const editProfile = (values) => {
+    // TODO: make editting profile possible
+    console.log(values);
+  };
+
+  const editNotifications = (values) => {
+    // TODO: make editting notifications possible
+    console.log(values);
+  };
+
+  // TODO: get profile info from database (if logged in, if not redirect, possibly through React router)
   const profile = {
     email: 'john.doe@gmail.com',
     firstName: 'John',
     lastName: 'Doe',
-    institution: 'Arteveldehogeschool'
+    institution: 'Arteveldehogeschool',
+    notifEmail: false,
+    notifApp: true
   };
+
   return (
     <>
       <Container>
         <Grid>
-          <div className="edit__profile">
-            <Panel>
-              <>
-                <Button
-                  onClick={() => Router.back()}
-                  text="Terug naar profiel"
-                  icon="arrow-left"
-                  reverse
-                />
+          <>
+            <div className="edit__profile">
+              <Panel>
+                <>
+                  <Button
+                    onClick={() => Router.back()}
+                    text="Terug naar profiel"
+                    icon="arrow-left"
+                    reverse
+                  />
 
-                <Heading title="Bewerk profiel" />
+                  <Heading title="Bewerk profiel" />
 
-                <Formik
-                  initialValues={{
-                    email: profile.email
-                  }}
-                  onSubmit={(values) => {
-                    // ...
-                  }}
-                >
-                  <Form>
-                    <InputField
-                      name="email"
-                      type="email"
-                      label="Email"
-                      value={profile.email}
-                      placeholder="E-mail adres"
-                    />
-                    <InputField
-                      name="firstname"
-                      type="text"
-                      label="Voornaam"
-                      value={profile.firstName}
-                      placeholder="Voornaam"
-                    />
-                    <InputField
-                      name="lastname"
-                      type="text"
-                      label="Familienaam"
-                      value={profile.lastName}
-                      placeholder="Familienaam"
-                    />
-                    <InputField
-                      name="institution"
-                      type="text"
-                      label="Organisatie/Onderwijsinstelling"
-                      value={profile.institution}
-                      placeholder="Organisatie/Onderwijsinstelling"
-                    />
-                    <Button
-                      // disabled={isSubmitting}
-                      text="Opslaan"
-                      type="submit"
-                      primary
-                    />
-                  </Form>
-                </Formik>
-              </>
-            </Panel>
-          </div>
-          <div className="edit__preferences">
-            <Heading marginTop title="Notificatie voorkeuren" level="2" />
-            <Formik
-              initialValues={{
-                email: profile.email
-              }}
-              onSubmit={(values) => {
-                // ...
-              }}
-            >
-              <Form>
-                <InputField
-                  name="institution"
-                  type="checkbox"
-                  label="Ontvang meldingen via e-mail"
-                />
-                <InputField
-                  name="institution"
-                  type="checkbox"
-                  label="Ontvang meldingen via de app"
-                />
+                  <Formik
+                    initialValues={{
+                      email: profile.email,
+                      firstName: profile.firstName,
+                      lastName: profile.lastName,
+                      institution: profile.institution
+                    }}
+                    onSubmit={(values) => {
+                      editProfile(values);
+                    }}
+                  >
+                    <Form>
+                      <InputField
+                        name="email"
+                        type="email"
+                        label="Email"
+                        placeholder="E-mail adres"
+                      />
+                      <InputField
+                        name="firstName"
+                        type="text"
+                        label="Voornaam"
+                        placeholder="Voornaam"
+                      />
+                      <InputField
+                        name="lastname"
+                        type="text"
+                        label="Familienaam"
+                        placeholder="Familienaam"
+                      />
+                      <InputField
+                        name="institution"
+                        type="text"
+                        label="Organisatie/Onderwijsinstelling"
+                        placeholder="Organisatie/Onderwijsinstelling"
+                      />
+                      <Button text="Opslaan" type="submit" primary />
+                    </Form>
+                  </Formik>
+                </>
+              </Panel>
+            </div>
+            <div className="edit__preferences">
+              <Heading marginTop title="Meldingsvoorkeuren" level={2} />
+              <p>{getNotifInfo('user')}</p>
+              <Formik
+                initialValues={{
+                  notifEmail: profile.notifEmail,
+                  notifApp: profile.notifApp
+                }}
+                onSubmit={(values) => {
+                  editNotifications(values);
+                }}
+              >
+                <Form>
+                  <InputField
+                    name="notifEmail"
+                    type="checkbox"
+                    label="Ontvang meldingen via e-mail"
+                  />
+                  <InputField
+                    name="notifApp"
+                    type="checkbox"
+                    label="Ontvang meldingen via de app"
+                  />
 
-                <Button text="Opslaan" type="submit" primary />
-              </Form>
-            </Formik>
-          </div>
+                  <Button text="Opslaan" type="submit" primary />
+                </Form>
+              </Formik>
+            </div>
+          </>
         </Grid>
       </Container>
 
