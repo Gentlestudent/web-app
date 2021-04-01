65c8d5db676989b34df9eea914415bf72e70771e
Ismaila Abdoulahi <36740618+auloin@users.noreply.github.com>
2021-04-01 13:47:12 +0200

    Fix

--- src/pages/api/badges/[id].js
+++ src/pages/api/badges/[id].js
@@ -3,8 +3,8 @@ import { getBadge } from '../../../api/functions';
 export default async function handler(req, res) {
   const { id } = req.query;
   try {
-    const badge = await getBadge(id);
-    return res.status(200).json(badge);
+    const { data } = await getBadge({ id });
+    return res.status(200).json(data);
   } catch (e) {
     if (e.code === 'not-found') return res.status(404).end();
     return res.status(500).end();
