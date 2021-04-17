4e731e9a36f05628b6a33c500e6d8f68a86d4801
Ismaila Abdoulahi <36740618+auloin@users.noreply.github.com>
2021-04-17 19:56:32 +0200

    Add custom auth action handler

---
+++ src/pages/auth/action.jsx
@@ -0,0 +1,31 @@
+import { useRouter } from 'next/router';
+import { useEffect, useReducer } from 'react';
+import { handleVerifyEmail } from '../../api/auth';
+import fetchStatusReducer from '../../reducers/fetchStatusReducer';
+
+const Action = () => {
+  const router = useRouter();
+  const [state, dispatch] = useReducer(fetchStatusReducer, {});
+
+  useEffect(() => {
+    const handleAction = async () => {
+      const { mode, oobCode } = router.query;
+
+      dispatch(['INIT']);
+      try {
+        if (mode === 'verifyEmail') {
+          const res = await handleVerifyEmail(oobCode);
+          console.log(res);
+          dispatch(['COMPLETE']);
+        }
+      } catch (error) {
+        dispatch(['ERROR', { code: `${mode.toLowerCase()}-fail` }]);
+      }
+    };
+    handleAction();
+  }, [router]);
+
+  return <></>;
+};
+
+export default Action;
