d5d3e914a9fcc8d1b5ad59c022e41d6efd050804
Ismaila Abdoulahi <36740618+auloin@users.noreply.github.com>
2021-04-23 20:26:14 +0200

    Show progress bar and hide page while user auth state is loading

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,69 +1,21 @@
-import { useEffect, useReducer } from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
-import Router from 'next/router';
-import NProgress from 'nprogress';
-import { useAuthState } from 'react-firebase-hooks/auth';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import 'react-quill/dist/quill.snow.css';
 
 import AuthContext from '../context/auth';
-import User from '../models/User';
-import { auth } from '../api/firebase';
-import { getProfile } from '../api/users';
 import { colors } from '../assets/styles';
 import Layout from '../components/layout';
 import globalStyles from '../assets/styles/global';
-import useExec from '../hooks/useExec';
-
-Router.events.on('routeChangeStart', () => {
-  NProgress.start();
-});
-Router.events.on('routeChangeComplete', () => NProgress.done());
-Router.events.on('routeChangeError', () => NProgress.done());
-
-function currentUserReducer(state, [type, payload]) {
-  switch (type) {
-    case 'auth':
-      return payload ? new User(payload) : null;
-    case 'data':
-      return state ? Object.assign(new User(state), payload) : null;
-    default:
-      return null;
-  }
-}
+import { useAuthState, useProgressBar } from '../hooks';
 
 const App = ({ Component, pageProps }) => {
-  const [user, authLoading, authError] = useAuthState(auth);
-  const [execLoading, execError, data, exec] = useExec(getProfile);
-  const [currentUser, dispatch] = useReducer(currentUserReducer, null);
-  const loading = authLoading || execLoading;
-  const error = authError || execError;
-
-  const reload = () => {
-    if (!loading && currentUser) {
-      exec(currentUser.id);
-    }
-  };
-
-  useEffect(() => {
-    if (!authLoading) {
-      dispatch(['auth', user]);
-      if (user) {
-        exec(user.uid);
-      }
-    }
-  }, [authLoading, user, exec]);
-
-  useEffect(() => {
-    if (!execLoading) {
-      dispatch(['data', data]);
-    }
-  }, [execLoading, data]);
+  const [loading, error, currentUser, reload] = useAuthState();
+  useProgressBar(loading);
 
   const authState = {
     authStatusReported: !loading,
-    isUserSignedIn: !!user,
+    isUserSignedIn: !!currentUser,
     error,
     currentUser,
     reload
