bf4f05adbc4d9a6f2d98a1963d301c2d1a63187c
Jo Jordens <jo@appsaloon.be>
2021-05-26 18:06:27 +0200

    build: disables firestore

--- src/api/issuers.js
+++ src/api/issuers.js
@@ -2,7 +2,8 @@ import { firestore } from './firebase';
 import { issuerConverter } from '../models/Issuer';
 
 export const getIssuerById = async (id) => {
-  return (
-    await firestore.collection('Issuers').doc(id).withConverter(issuerConverter).get()
-  ).data();
+  return {};
+  // return (
+  //   await firestore.collection('Issuers').doc(id).withConverter(issuerConverter).get()
+  // ).data();
 };
