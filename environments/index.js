b2002caa1618a843ee6645c95cbc6a7bee69ab3e
Jo Jordens <jo@appsaloon.be>
2021-05-26 18:16:53 +0200

    build: gets env variables from env instead of files

--- environments/index.js
+++ environments/index.js
@@ -1,18 +1,30 @@
-const environments = { development: 'dev', production: 'production' };
+// const environments = { development: 'dev', production: 'production' };
 
-module.exports = (env) => {
-  let environment = {};
+// module.exports = (env) => {
+//   let environment = {};
 
-  try {
-    // eslint-disable-next-line
-    environment = require(`./env.${environments[env]}`);
-  } catch (ex) {
-    if (ex instanceof Error && ex.code === 'MODULE_NOT_FOUND') {
-      console.log(
-        `Couldn't load environment file. Make sure an environment file for "${process.env.NODE_ENV}" exists under /environments`
-      );
-      process.exit(1);
-    } else throw ex;
-  }
-  return environment;
-};
+//   try {
+//     // eslint-disable-next-line
+//     environment = require(`./env.${environments[env]}`);
+//   } catch (ex) {
+//     if (ex instanceof Error && ex.code === 'MODULE_NOT_FOUND') {
+//       console.log(
+//         `Couldn't load environment file. Make sure an environment file for "${process.env.NODE_ENV}" exists under /environments`
+//       );
+//       process.exit(1);
+//     } else throw ex;
+//   }
+//   return environment;
+// };
+
+module.exports = () => ({
+  FIREBASE: {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+  },
+  USE_FUNCTIONS_EMULATOR: process.env.FIREBASE_USE_FUNCTIONS_EMULATOR
+});
