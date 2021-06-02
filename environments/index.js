b23c9c615e7711637f39f7a5c22626b2b43b6379
Jo Jordens <jo@appsaloon.be>
2021-06-02 17:00:05 +0200

    feat: adds new env variables

--- environments/index.js
+++ environments/index.js
@@ -26,5 +26,22 @@ module.exports = () => ({
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.FIREBASE_APP_ID
   },
-  USE_FUNCTIONS_EMULATOR: process.env.FIREBASE_USE_FUNCTIONS_EMULATOR
+  USE_FUNCTIONS_EMULATOR: process.env.FIREBASE_USE_FUNCTIONS_EMULATOR,
+  FIREBASE_ADMIN: {
+    type: process.env.FIREBASE_ADMIN_TYPE,
+    project_id: process.env.FIREBASE_ADMIN_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_ADMIN_AUTH_URI,
+    token_uri: process.env.FIREBASE_ADMIN_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_ADMIN_CLIENT_X509_CERT_URL
+  },
+  DATABASE_DATABASE: process.env.DATABASE_DATABASE,
+  DATABASE_USERNAME: process.env.DATABASE_USERNAME,
+  DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
+  DATABASE_HOST: process.env.DATABASE_HOST,
+  POSTMARK_TOKEN: process.env.POSTMARK_TOKEN
 });
