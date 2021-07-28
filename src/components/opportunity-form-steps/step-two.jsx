726d8fbf9864db313d7a4a3ac299e23012f2cc01
Jo Jordens <jo@appsaloon.be>
2021-07-28 12:13:21 +0200

    feat: adds min & max to 'create opportunity' date pickers

--- src/components/opportunity-form-steps/step-two.jsx
+++ src/components/opportunity-form-steps/step-two.jsx
@@ -1,11 +1,14 @@
+import { useFormikContext } from 'formik';
 import { InputField } from '../form';
 
 const StepTwo = () => {
+  const { values } = useFormikContext();
+
   return (
     <>
-      <InputField name="startDate" type="date" label="Start datum" />
+      <InputField name="startDate" type="date" label="Start datum" max={values.endDate || ''} />
 
-      <InputField name="endDate" type="date" label="Eind datum" />
+      <InputField name="endDate" type="date" label="Eind datum" min={values.startDate || ''} />
 
       <InputField name="website" type="text" label="Website" placeholder="Website url" />
 
